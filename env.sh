60644e3b1215c2eb8926dd1ef87ac4847edec60d
Vadym Hevlich <vege1wgw@gmail.com>
2019-03-04 15:23:52 +0200

    env.sh fixed

--- env.sh
+++ env.sh
@@ -2,8 +2,8 @@
 
 export ALLOWED_HOSTS='*'
 export SECRET_KEY='huxtio*k7xptq8qc5i-(o4(6-_@+&-ot^0y3^5%np3+o0f5o78'
-export DATABASE_NAME='webinars'
-export DATABASE_USER='hevlich'
+export DATABASE_NAME='webinar_dev'
+export DATABASE_USER='webinar_user'
 export DATABASE_PASSWORD='pass12pass'
 export DATABASE_HOST='127.0.0.1'
 export DATABASE_PORT='5432'
