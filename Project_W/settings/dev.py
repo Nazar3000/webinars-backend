86f84cf3adeee7e8cb0a17c57d7e0085f88b0b2d
Vadym Hevlich <vege1wgw@gmail.com>
2019-03-06 16:53:12 +0200

    prod mailing configs

--- Project_W/settings/dev.py
+++ Project_W/settings/dev.py
@@ -2,9 +2,9 @@ from .base import *
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_HOST = 'localhost'
-EMAIL_HOST_PASSWORD = ""
-EMAIL_HOST_USER = 'no-reply@foxery.io'
-EMAIL_PORT = 25
-EMAIL_USE_TLS = False
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = "<rz`JG_st'y23j[L"
+EMAIL_HOST_USER = 'projectw.service@gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
