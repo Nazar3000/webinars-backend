b536b3fa729d8deb81d3b9e97d9b474aa0e22016
Vadym Hevlich <vege1wgw@gmail.com>
2019-03-21 12:11:52 +0200

    prod chat template dir added

--- Project_W/settings/production.py
+++ Project_W/settings/production.py
@@ -8,3 +8,5 @@ EMAIL_HOST_USER = 'no-reply@foxery.io'
 EMAIL_PORT = 25
 EMAIL_USE_TLS = False
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+TEMPLATES[0]['DIRS'] += [os.path.join(BASE_DIR, 'chat/templates')]
