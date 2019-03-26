7b7b87061c40352f5fa5a6925a047fd675a99594
Vadym Hevlich <vege1wgw@gmail.com>
2019-03-26 20:23:50 +0200

    telegram bot interface done

--- chains/helpers.py
+++ chains/helpers.py
@@ -20,49 +20,12 @@ def bot_init(api_key, bot_pk):
     # updater.idle()
 
 
-def get_msg_type_and_content(message):
-    msg_type = None
-    content = None
-    if message.text:
-        msg_type = 'text'
-        content = message.text
-    elif message.link:
-        msg_type = 'link'
-        content = message.link
-    elif message.image:
-        msg_type = 'image'
-        content = message.image
-    elif message.audio:
-        msg_type = 'audio'
-        content = message.audio
-    elif message.video:
-        msg_type = 'video'
-        content = message.video
-    elif message.file:
-        msg_type = 'file'
-        content = message.file
-    elif message.map:
-        msg_type = 'map'
-        content = message.map
-    # elif message.timer:
-    #     msg_type = 'timer'
-    # elif message.button:
-    #     msg_type = 'button'
-    return msg_type, content
-
-
 def start_telegram_chain(chain, bot):
     messages = chain.chains_message.all().order_by('order')
     for message in messages:
-        msg_type, content = get_msg_type_and_content(message)
-        if msg_type and content:
-            if message.delay == 0:
-                print('no delay')
-                send_bot_message.delay(content, msg_type, bot.api_key, bot.pk, bot.chat_id)
-            else:
-                send_bot_message.apply_async(
-                    [content, msg_type, bot.api_key, bot.pk, bot.chat_id],
-                    countdown=message.delay)
+        send_bot_message.apply_async(
+            [message.pk, bot.api_key, bot.pk, bot.telegram_chat_id],
+            countdown=message.delay)
 
 
 def start_bot_chain(chain, bot):
--- chains/tasks.py
+++ chains/tasks.py
@@ -3,14 +3,66 @@ from telegram import Bot
 from Project_W.celery import app
 
 
+def get_msg_type_and_content(message):
+    msg_type = None
+    content = None
+    if message.text:
+        msg_type = 'text'
+        content = message.text
+    elif message.link:
+        msg_type = 'link'
+        content = message.link
+    elif message.image:
+        msg_type = 'image'
+        content = message.image
+    elif message.audio:
+        msg_type = 'audio'
+        content = message.audio
+    elif message.video:
+        msg_type = 'video'
+        content = message.video
+    elif message.file:
+        msg_type = 'file'
+        content = message.file
+    elif message.map:
+        msg_type = 'map'
+        content = message.map
+    # elif message.timer:
+    #     msg_type = 'timer'
+    # elif message.button:
+    #     msg_type = 'button'
+    return msg_type, content
+
+
 @app.task
-def send_bot_message(content, msg_type, api_key, bot_pk, chat_id):
-    print('inside')
+def send_bot_message(msg_pk, api_key, bot_pk, chat_id):
     from bots.models import BotBase
-    print(content, msg_type, api_key, bot_pk)
+    from chains.models import Message
 
     bot_instance = BotBase.objects.get(pk=bot_pk)
+    message = Message.objects.get(pk=msg_pk)
+    msg_type, content = get_msg_type_and_content(message)
+
     if bot_instance.telegram_chat_id:
         bot = Bot(api_key)
 
-        bot.send_message(chat_id=chat_id, text=content)
+        if msg_type == 'text':
+            bot.send_message(chat_id=chat_id, text=content)
+        elif msg_type == 'link':
+            bot.send_message(
+                chat_id=chat_id,
+                text='[{content}]({content})'.format(content=content), parse_mode='markdown')
+        elif msg_type == 'image':
+            bot.send_photo(chat_id=chat_id, photo=content)
+        elif msg_type == 'audio':
+            bot.send_audio(chat_id=chat_id, audio=content)
+        elif msg_type == 'video':
+            bot.send_video(chat_id=chat_id, video=content)
+        elif msg_type == 'file':
+            bot.send_document(chat_id=chat_id, document=content)
+        elif msg_type == 'map':
+            bot.send_location(chat_id=chat_id, latitude=content[0], longitude=content[1])
+        # elif msg_type == 'timer':
+        #     pass
+        # elif msg_type == 'button':
+        #     pass
